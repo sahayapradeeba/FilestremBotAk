6d955ff6edd35b545789812d824bff8ed26a3815
Sahaya Pradeeba T <131452422+sahayapradeeba@users.noreply.github.com>
2024-07-28 17:53:55 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -8,10 +8,10 @@ load_dotenv()
 class Telegram:
     API_ID = int(env.get("API_ID", "22225617"))
     API_HASH = str(env.get("API_HASH", "ef16f7597376f1689663304c954e4493"))
-    BOT_TOKEN = str(env.get("BOT_TOKEN", "7242317141:AAGAIjdTlThKtArXmB0bD7bDMmus10Ur6G0"))
-    OWNER_ID = int(env.get('OWNER_ID', '6072149828'))
+    BOT_TOKEN = str(env.get("BOT_TOKEN", "7375854453:AAHbjPUQhVOZ7IAF1tjyiciEtJdFDBYgbB8"))
+    OWNER_ID = int(env.get('OWNER_ID', '1473415982'))
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    DATABASE_URL = str(env.get('DATABASE_URL','mongodb+srv://akmonsterprogrammer:S.Aruna1155182089@filetolink.erqitim.mongodb.net/?retryWrites=true&w=majority&appName=filetolink'))
+    DATABASE_URL = str(env.get('DATABASE_URL','mongodb+srv://TheatrePrint77:TheatrePrint77@filesharexbot.bjbua6q.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
     FORCE_SUB_ID = env.get('FORCE_SUB_ID', "-1002145921498")
@@ -22,8 +22,8 @@ class Telegram:
     START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
     MULTI_CLIENT = True
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", "-1002053122232"))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", "-1002053122232"))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", "-1007375854453"))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", "-1007375854453"))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "6072149828")).split()))
